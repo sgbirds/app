24187e4a963d906510ac14d48f86c12ba906beb5
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-10-23 16:24:52 +0800

    Add date picker

--- src/App.js
+++ src/App.js
@@ -6,15 +6,24 @@ import logo from "./logo.png";
 class App extends Component {
   constructor(prop) {
     super(prop);
+
     this.state = {
+      date: this.getCurrentDate(),
       contributors: []
     };
   }
 
   async componentDidMount() {
+    await this.fetchTopContributions();
+  }
+
+  async fetchTopContributions() {
     try {
       const baseUrl = "https://api.ebird.org/v2";
-      const url = `${baseUrl}/product/top100/SG/2019/10/21`;
+      const date = this.state.date.split("-").join("/");
+      const url = `${baseUrl}/product/top100/SG/${date}`;
+
+      console.log(url);
 
       const headers = {
         "X-eBirdApiToken": process.env.REACT_APP_EBIRD_API_TOKEN
@@ -28,7 +37,18 @@ class App extends Component {
     }
   }
 
-  addProfileLink(contributor) {
+  getCurrentDate() {
+    const d = new Date(Date.now());
+    return [d.getFullYear(), d.getMonth() + 1, d.getDate()].join("-");
+  }
+
+  datePicker = event => {
+    this.setState({date: event.target.value}, () => {
+      this.fetchTopContributions();
+    });
+  };
+
+  renderProfileLink(contributor) {
     if (contributor && contributor.profileHandle) {
       return (
         <Fragment>
@@ -43,20 +63,29 @@ class App extends Component {
     }
   }
 
+  renderTopContributions() {
+    if (this.state.contributors.length === 0) {
+      return <h2>No records found for {this.state.date}</h2>;
+    }
+
+    return this.state.contributors.map(contributor => (
+      <div className="contributor" key={contributor.userId}>
+        <h2>
+          {contributor.userDisplayName}
+          {this.renderProfileLink(contributor)}
+        </h2>
+        <p>Number of species observed: {contributor.numSpecies}</p>
+      </div>
+    ));
+  }
+
   render() {
     return (
       <div className="App">
         <img className="logo" alt="logo" src={logo} />
         <h1>Top Contributors in Singapore</h1>
-        {this.state.contributors.map(contributor => (
-          <div className="contributor" key={contributor.userId}>
-            <h2>
-              {contributor.userDisplayName}
-              {this.addProfileLink(contributor)}
-            </h2>
-            <p>Number of species observed: {contributor.numSpecies}</p>
-          </div>
-        ))}
+        <input type="date" onChange={this.datePicker} value={this.state.date} />
+        {this.renderTopContributions()}
       </div>
     );
   }
